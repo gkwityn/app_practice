bfead4a314598d314ac853d0f314a3cef8f678c0
gkwityn <gkwityn@gmail.com>
2020-02-28 16:54:50 -0500

    add initial sorting

--- Bin2Dec/bin2dec.js
+++ Bin2Dec/bin2dec.js
@@ -1,3 +1,5 @@
+
+
 function submitValue(){
     var input = document.getElementById("binaryInput").value;
         
@@ -6,16 +8,11 @@ function submitValue(){
     validInput = validateForm(input);
 
     if(validInput)
-        calculateBinary(input);
-    
-    //  Input Validation:
-    //
-    //  11111111
-    //  10101010
-    //  12341234
-    //  abc
-    //  ""
-}   
+        calculateBinary(input);    
+}
+
+
+
 
 
 function validateForm(input){
@@ -67,4 +64,63 @@ function calculateBinary(input){
     
     //Print Decimal result
     document.getElementById("decimalOutput").value = total;
+}
+
+function calculateBinaryTest(input){
+    var total = 0;
+    var exponent = 0;
+
+    for(var j = input.length - 1; j >=0; j--){
+        if(parseInt(input.charAt(j)) === 1){
+            total += Math.pow(2, exponent);
+        }
+        exponent++;
+    }
+    
+    //Print Decimal result
+    document.getElementById("decimalOutput").value = total;
+}
+
+
+function testSuit(){
+
+    var tests = [
+        "",
+        11111111, 10101010,
+        12341234, 10000000,
+        10000001, 10000010,
+        10000011, 10000100,
+        10000101, 10000110,
+        10000111, 10001000,
+        10001001, 10001010,
+        10001011, 10001100,
+        10001101, 10001110,
+        10001111, 10010000,
+        10010001, 10010010,
+        10010011, 10010100,
+        10010101, 10010110,
+        10010111, 10011000,
+        10011001, 10011010,
+        10011011, 10011100,
+        10011101, 10011110,
+        10011111, 10100000,
+        10100001, 10100010,
+        10100011, 10100100,
+        10100101, 10100110,
+        10100111, 10101000,
+        10101001, 10101010,
+        10101011, 10101100
+    ];
+
+    for(let x = 0; x < 100; x++){
+        let r = Math.random().toString(36).substring(9);
+        tests.push(r);        
+    }
+
+    for(let x = 0; x < tests.length - 1; x++){
+        if(validateForm( tests[i]) ){
+            calculateBinaryTest(test[i]);
+        }
+    }
+
 }
